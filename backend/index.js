055801871f0a5f91689979e7b3e9457cf4b60e7f
pawan <pawanpatidar137@gmail.com>
2023-06-20 22:27:24 +0530

    write database connected code in backend

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
-
+require("./db/config");
+const User = require("./db/users");
 const app = express();
-
-app.get("/", (req, res) => {
-  res.send("app is running......");
+app.use(express.json());
+app.post("/register", async (req, res) => {
+  let user = new User(req.body);
+  let result = await user.save();
+  res.send(result);
 });
 
-app.listen(5000);
+app.listen(8000);
