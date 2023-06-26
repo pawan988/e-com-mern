4a5feb83e444a672f18387fc5e8be9c538a2dd0f
pawan <pawanpatidar137@gmail.com>
2023-06-26 22:32:42 +0530

    solved the cors error

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 require("./db/config");
+const cors = require("cors");
 const User = require("./db/users");
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.post("/register", async (req, res) => {
+  console.log("reqqqq ===>>>>", req);
   let user = new User(req.body);
   let result = await user.save();
   res.send(result);
