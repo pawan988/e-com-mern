b654e2b2a81cff6d40f858536f312060b586c6f4
pawan <pawanpatidar137@gmail.com>
2023-06-28 21:43:49 +0530

    create login API

--- backend/index.js
+++ backend/index.js
@@ -7,10 +7,24 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.post("/register", async (req, res) => {
-  console.log("reqqqq ===>>>>", req);
   let user = new User(req.body);
   let result = await user.save();
+  result = result.toObject();
+  delete result.password;
   res.send(result);
+});
+
+app.post("/login", async (req, res) => {
+  if (req.body.email && req.body.password) {
+    let user = await User.findOne(req.body).select("-password");
+    if (user) {
+      res.send(user);
+    } else {
+      res.send({ message: "User dose not exist" });
+    }
+  } else {
+    res.send({ message: "Bad Request" });
+  }
 });
 
 app.listen(8000);
